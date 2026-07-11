767f67e2d8575100262469434aa9365a066a833d
Junithaayu <junithayuuu@gmail.com>
2026-07-11 20:31:52 +0700

    Test create route

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,7 +1,25 @@
 <x-app-layout>
     <x-slot name="header">
-        Tambah Produk
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Tambah Produk
+        </h2>
     </x-slot>
 
-    HALO
+    <div class="py-6">
+        <div class="max-w-3xl mx-auto">
+            <div class="bg-white shadow rounded-lg p-6">
+
+                HALO
+
+                <form method="POST">
+                    @csrf
+
+                    <button type="submit">
+                        Simpan
+                    </button>
+                </form>
+
+            </div>
+        </div>
+    </div>
 </x-app-layout>
